ddfd282c471fce3ff6662984f1e015017cdd2904
rakibul-islam-github <freelancing.everyday@gmail.com>
2022-04-16 14:32:00 +0600

    dashboard header added

---
+++ src/components/Dashboard/DashboardHeader.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const DashboardHeader = () => {
+    
+    
+      return (
+        <header
+          id="header"
+          className="fixed-top d-flex align-items-center header-transparent"
+        >
+          <div className=" d-flex align-items-center header-div">
+            <h1 className="logo me-auto">
+              <NavLink exact to="/">
+                Bitx
+              </NavLink>
+            </h1>
+    
+            <nav id="navbar" className="navbar order-last order-lg-0">
+              <ul>
+                
+                <li>
+                    <NavLink exact to="#"
+                  className="nav-link scrollto">
+                    Terra
+                  
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    exact
+                    
+                    to="/dashboard"
+                    className="nav-link scrollto"
+                  >
+                    Username
+                  </NavLink>
+                </li>
+              </ul>
+              <i className="bi bi-list mobile-nav-toggle"></i>
+            </nav>
+          </div>
+        </header>
+      );
+};
+
+export default DashboardHeader;
